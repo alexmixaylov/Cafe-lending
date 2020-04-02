d27c73eb6d109256b1c719ad79edaebeb1e204c8
Danik <daniknewgarden@gmail.com>
2020-04-02 15:37:18 +0300

    Aded more divs with content

--- js/scripts.js
+++ js/scripts.js
@@ -10,9 +10,19 @@ window.addEventListener('DOMContentLoaded', function name() {
 
         menuBtn.classList.toggle('opened');
         menu.classList.toggle('hidden');
-
+        body.classList.toggle('lock-scroll');
     }
 
     menuBtn.addEventListener('click', openMenu);
 
+
+    // let select = document.querySelectorAll('.select');
+
+
+    // let option = select[1].querySelectorAll('.option');
+
+    // for (let i = 0; i < option.length; i++) {
+    //     console.log(option[i].value);
+        
+    // }
 });
