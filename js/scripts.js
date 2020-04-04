0141a774ab3300452c819c0c545ba1ac12e7ab48
Danik <daniknewgarden@gmail.com>
2020-04-04 22:40:59 +0300

    Aded scripts for smooth scrool

--- js/scripts.js
+++ js/scripts.js
@@ -1,55 +1,73 @@
 window.addEventListener('DOMContentLoaded', function name() {
 
-    let body = document.body;
-        menuBtn = document.querySelector('.burger-button');
+    let menuBtn = document.querySelector('.burger-button');
 
     
     function openMenu() {
 
-        let menu = document.querySelector('.menu');
+        let menu = document.querySelector('.menu'),
+            body = document.body,
+            menuLink = document.querySelectorAll('.menu-links .link');
 
         menuBtn.classList.toggle('opened');
         menu.classList.toggle('hidden');
         body.classList.toggle('lock-scroll');
+
+        for (let i = 0; i < menuLink.length; i++) {
+            menuLink[i].addEventListener('click', function () {
+                menuBtn.classList.remove('opened');
+                menu.classList.add('hidden');
+                body.classList.remove('lock-scroll');
+            })
+        }
+        
     }
 
     menuBtn.addEventListener('click', openMenu);
 
-    (function () {
-        var app = "https://script.googleusercontent.com/macros/echo?user_content_key=MsGGy8yAlqh1g7vrrazxb4KcQJ2FdlgcYJlTTMVt3p_FroUT11Ss1JqU3to-jK3owsz4AFpX-3jCGmSZPSRrPBd6i5a1kM7um5_BxDlH2jW0nuo2oDemN9CCS2h10ox_1xSncGQajx_ryfhECjZEnE6ILO7PDLX1N-1y1prnoGsV_sSvwp76DSDLyIQ62MXIi8bDz9j3YN6ALj5NvI9ZeIp-WDTslP8h&lib=MR_ogYSPEiyh-NPOYOLj_-cBLav_N1cup",
-            output = '',
-            xhr = new XMLHttpRequest();
-        xhr.open('GET', app);
-        xhr.onreadystatechange = function () {
-            if (xhr.readyState !== 4) return;
+    function anchorLinkHandler(e) {
+        const distanceToTop = el => Math.floor(el.getBoundingClientRect().top);
 
-            if (xhr.status == 200) {
-                try {
-                    var r = JSON.parse(xhr.responseText),
-                        result = r["result"];
-                    for (var i = 0; i < result.length; i++) {
-                        var obj = r["result"][i];
-                        output += obj.join("<br/>") + "<br/><hr/>";
-                    }
-                } catch (e) {}
+        e.preventDefault();
+        const targetID = this.getAttribute("href");
+        const targetAnchor = document.querySelector(targetID);
+        if (!targetAnchor) return;
+        const originalTop = distanceToTop(targetAnchor);
+
+        window.scrollBy({
+            top: originalTop,
+            left: 0,
+            behavior: "smooth"
+        });
+
+        const checkIfDone = setInterval(function () {
+            const atBottom = window.innerHeight + window.pageYOffset >= document.body.offsetHeight - 2;
+            if (distanceToTop(targetAnchor) === 0 || atBottom) {
+                targetAnchor.tabIndex = "-1";
+                targetAnchor.focus();
+                window.history.pushState("", "", targetID);
+                clearInterval(checkIfDone);
             }
+        }, 100);
+    }
 
-            document.getElementById('info').innerHTML = output;
+    const linksToAnchors = document.querySelectorAll('a[href^="#"]');
 
-        }
-        xhr.send()
-    })()
+    linksToAnchors.forEach(each => (each.onclick = anchorLinkHandler));
 
-    // function resizeTitle() {
-    //     let image = document.querySelectorAll('.lol'),
-    //         title = document.querySelectorAll('.azaz');
+    function changeMenuBg(top, height) {
 
-    //         for (let i = 0; i < image.length; i++) {
-    //             title[i].getElementsByClassName.height = image[i].scrollHeight;
-    //             console.log('ok');
-    //         }
+        let header = document.querySelector('.header').getBoundingClientRect().top;
 
-    // }
+        top = window.scrollTop;
+        heigth = document.querySelector('.main-content').clientHeight;
 
-    // resizeTitle();
+        // setInterval(() => {
+        //     console.log(window);
+        // }, 100);
+            
+    }
+    
+    changeMenuBg();
+
 });
