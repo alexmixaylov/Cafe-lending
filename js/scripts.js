3f233d950c2eb216d680d1afc1b9727fbd99cf79
Danik <daniknewgarden@gmail.com>
2020-04-21 22:50:12 +0300

    Aded slides switch before swipe on mobile device

--- js/scripts.js
+++ js/scripts.js
@@ -20,7 +20,7 @@ window.addEventListener('DOMContentLoaded', function name() {
                 menuBtn.classList.remove('opened');
                 menu.classList.add('hidden');
                 body.classList.remove('lock-scroll');
-            })
+            });
         }
         
     }
@@ -102,7 +102,7 @@ window.addEventListener('DOMContentLoaded', function name() {
     const nextBtn = document.querySelector('.next-slide'),
           prevBtn = document.querySelector('.prev-slide'),
           slides = document.querySelectorAll('.slider-slide'),
-          pagination = document.querySelectorAll('.pagination');
+          pagination = document.querySelectorAll('.round');
 
     let active = 0;
 
@@ -144,5 +144,49 @@ window.addEventListener('DOMContentLoaded', function name() {
 
     nextBtn.addEventListener('click', nextSlide);
     prevBtn.addEventListener('click', prevSlide);
+    
+    //Autoplay
+
+    setInterval(() => {
+        nextSlide();
+    }, 5000);
+
+    //Swipe control
+
+    document.addEventListener('touchstart', handleTouchStart, false);
+    document.addEventListener('touchmove', handleTouchMove, false);
+
+    var xDown = null;
+    var yDown = null;
+
+    function getTouches(evt) {
+        return evt.touches || // browser API
+            evt.originalEvent.touches; // jQuery
+    }
+
+    function handleTouchStart(evt) {
+        const firstTouch = getTouches(evt)[0];
+        xDown = firstTouch.clientX;
+    }
+
+    function handleTouchMove(evt) {
+        if (!xDown) {
+            return;
+        }
+
+        var xUp = evt.touches[0].clientX;
+
+        var xDiff = xDown - xUp;
+
+            if (xDiff > 0) {
+                prevSlide();
+            } else {
+                nextSlide();
+            }
+
+        /* reset values */
+        xDown = null;
+        yDown = null;
+    }
 
 });
